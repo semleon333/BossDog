d32559586bdc6c26e73cb5dc9888da4c8b35f2c3
Dandead <61425314+Dandead@users.noreply.github.com>
2022-04-22 22:00:00 +0300

    WIP

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,18 +1,22 @@
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# Setup environments variables
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv("DJANGO_DEBUG") in ["True", "true", True]:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -98,11 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Moscow'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
--- src/telegram_bot/models.py
+++ src/telegram_bot/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+# users prototype for tg bot
+# class TelegramUser(models.Model):
+#     user_id = models.IntegerField("Telegram id", primary_key=True)
+#     username = models.CharField("Username", max_length=32, null=True, blank=True)
+#     first_name = models.CharField("First name", max_length=256)
+#     last_name = models.CharField("Second name", max_length=256, null=True, blank=True)
+#     language_code = models.CharField("User lang", max_length=8, null=True, blank=True)
+#
+#     is_blocked_bot = models.BooleanField(default=False)
+#     is_banned = models.BooleanField(default=False)
+#
+#     is_admin = models.BooleanField(default=False)
+#     is_moderator = models.BooleanField(default=False)
+#
+#     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     class Meta:
+#         db_table = 'telegram_users'
+#         ordering = ('-created_at',)
+#         verbose_name = 'TG user'
+#         verbose_name_plural = 'TG users'
+#
+#     def __str__(self):
+#         return self.username if self.username else str(self.user_id)
+#
+#     @property
+#     def deep_link(self):
+#         return f't.me/{self.username}' if self.username else None
