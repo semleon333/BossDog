b281ea43931a28a3c63cde9922f06d00a6225150
Dandead <61425314+Dandead@users.noreply.github.com>
2022-04-26 17:18:43 +0300

    Completed models of products and materials

---
+++ src/README.md
@@ -0,0 +1,41 @@
+
+# Set up project for tests:
+
+> #### There are two ways to do that:
+>
+>1. Using PostgreSQL
+>2. Using SQLite
+
+### 1. Config with PostgreSQL
+
+Clone this repo and switch to Django-dev branch with this command:
+
+    git clone -b django_setup https://github.com/semleon333/BossDog.git && cd BossDog/src
+
+Install and run postgresql on your server:
+
+    sudo apt install postgresql postgresql-contrib
+    sudo systemctl start postgresql.service
+
+Then rename **.env.dist** to **.env** and replace all values in this file with yours.
+
+After that, get PostgreSQL shell:
+
+    sudo -u postgres psql postgres
+
+and configure database and user (all values in "<>" must match with .env):
+
+    CREATE USER <DB_USER> WITH PASSWORD '<DB_PASSWORD>';
+    CREATE DATABASE <DB_NAME> OWNER <DB_USER>;
+    GRANT ALL ON DATABASE <DB_NAME> TO <DB_USER>;
+    \q
+    
+
+Create virtual environment and install dependencies:
+
+    python -m venv venv && . ./venv/bin/activate && pip install -r requirements.txt
+
+Then make django migrations and start server:
+
+    ./manage.py makemigrations && ./manage.py migrate
+    ./manage.py runserver
--- src/api/models.py
+++ src/api/models.py
@@ -5,28 +5,55 @@ from utils import models as utils_mod
 class Warehouse(models.Model):
 	"""Таблица с предметами на складе"""
 	name = models.CharField('Название', max_length=256)
-	color = models.CharField('Цвет(опц)', max_length=32, blank=True, null=True)
-	material = models.CharField('Материал(опц)', max_length=64, blank=True, null=True)
-	diameter = models.DecimalField('Диаметр(опц)', decimal_places=2, blank=True, null=True)
-	length = models.DecimalField('Длина(опц)', decimal_places=2, blank=True, null=True)
-	width = models.DecimalField('Ширина(опц)', decimal_places=2, blank=True, null=True)
+	color = models.CharField('Цвет', max_length=32, blank=True, null=True)
+	material = models.CharField('Материал', max_length=64, blank=True, null=True)
+	diameter = models.DecimalField('Диаметр, мм', max_digits=10, decimal_places=2, blank=True, null=True)
+	length = models.DecimalField('Длина, мм', max_digits=10, decimal_places=2, blank=True, null=True)
+	width = models.DecimalField('Ширина, мм', max_digits=10, decimal_places=2, blank=True, null=True)
 	amount = models.IntegerField('Количество')
 	
 	class Meta:
 		db_table = "warehouse"
+		verbose_name = "Материал"
+		verbose_name_plural = "Материалы"
+	
+	def __str__(self):
+		if self.diameter and not (self.length or self.width):
+			return f'{self.name}-{self.diameter}'
+		if self.length and self.width:
+			return f'{self.name}-{self.length}*{self.width}'
+		else:
+			return self.name
 
 
 class Products(models.Model):
 	"""Таблица с предлагаемыми товарами"""
 	name = models.CharField('Название', max_length=256)
 	description = models.TextField('Описание', blank=True, null=True)
-	materials = models.ManyToManyField('warehouse')
-	cost = models.DecimalField('Стоимость(опц)', decimal_places=2, blank=True, null=True)
-	image = models.ImageField(upload_to=utils_mod.media_namer, null=True)
+	materials = models.ManyToManyField(Warehouse, verbose_name='Материалы')
+	cost = models.DecimalField('Стоимость, руб.', max_digits=10, decimal_places=2, blank=True, null=True)
 	
 	class Meta:
 		db_table = "products"
+		verbose_name = "Товар"
+		verbose_name_plural = "Товары"
+	
+	def __str__(self):
+		return self.name
 
+	
+class ProductPhotos(models.Model):
+	"""Таблица для фотографий товара"""
+	fk = models.ForeignKey(Products, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to=utils_mod.media_namer, verbose_name='Фото товара')
+
+	class Meta:
+		db_table = "product_photos"
+		verbose_name = "Фото товара"
+		verbose_name_plural = "Фотографии товара"
+
+	def __str__(self):
+		return "Фото товара"
 
 # class Orders(models.Model):
 # 	"""Таблица для заказов"""   # Нужно сделать, когда прикрутится телега для указателей на ID
