b1377d74b28717d91805f1d285b50709e575b08b
semleon333 <76815011+semleon333@users.noreply.github.com>
2022-05-04 10:59:33 +0300

    src/install.sh: refactoring

--- src/install.sh
+++ src/install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 
-# installing base OS packeges
-sudo apt install postgresql postgresql-contrib
-sudo apt install python3 python3-pip python3-venv python3-dev libpq-dev
+# installing base OS packeges: PostgresSQL, Python
+sudo apt install postgresql postgresql-contrib python3 python3-pip python3-venv python3-dev libpq-dev
 # start PostgreSQL deamon
 sudo systemctl start postgresql.service
 
@@ -11,19 +10,19 @@ sudo systemctl start postgresql.service
 # entering data
 echo "Enter username(NOT 'user') of db_user:"
 read DB_USER
-echo "You enter: '$DB_USER'"
+echo "You enter: '$DB_USER'\n"
 
 echo "Enter password of db_user:"
 read DB_PASSWORD
-echo "You enter: '$DB_PASSWORD'"
+echo "You enter: '$DB_PASSWORD'\n"
 
 echo "Enter db_name:"
 read DB_NAME
-echo "You enter: '$DB_NAME'"
+echo "You enter: '$DB_NAME'\n"
 
 echo "Enter Django SECRET_KEY:"
 read SECRET_KEY
-echo "You enter: '$SECRET_KEY'"
+echo "You enter: '$SECRET_KEY'\n"
 
 
 # psql
@@ -33,11 +32,12 @@ sudo -u postgres psql postgres -c "GRANT ALL ON DATABASE $DB_NAME TO $DB_USER;"
 
 
 # generate .env
-echo "DB_NAME=$DB_NAME" >> .env
-echo "DB_USER=$DB_USER" >> .env
-echo "DB_PASSWORD=$DB_PASSWORD" >> .env
-echo "SECRET_KEY=$SECRET_KEY" >> .env
-echo "DJANGO_DEBUG=True" >> .env
+echo "
+DB_NAME=$DB_NAME
+DB_USER=$DB_USER
+DB_PASSWORD=$DB_PASSWORD
+SECRET_KEY=$SECRET_KEY
+DJANGO_DEBUG=True" >> .env
 
 
 # venv and requirments
